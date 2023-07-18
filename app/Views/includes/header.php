83494b51f9910d78e2248413c2358a6078dac7c2
Marcosh22 <marcos.henriquerds@gmail.com>
2023-07-18 09:25:53 -0300

    feat: added gtag

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -69,6 +69,16 @@
         }
     }  ?>
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6NK6ERGGE"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Z6NK6ERGGE');
+        </script>
+
         <?php if(isset($general) && isset($general->head_html) && !empty($general->head_html)) { ?>
         <?= $general->head_html; ?>
         <?php } ?>
