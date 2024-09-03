624090bb2276a5b3f3bbcd586bb1659f41e133d6
Marcosh22 <marcos.henriquerds@gmail.com>
2024-09-03 10:51:16 -0300

    feat: upd lp

--- app/Views/landing-pages/metamorfose.php
+++ app/Views/landing-pages/metamorfose.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Missão Metamorfose - Copercana</title>
-    <link rel="stylesheet" href="<?= base_url("_assets/landing-pages/metamorfose/css/style.css") ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/css/style.css") ?>">
+    <link rel="stylesheet"
+        href="<?= base_url("_assets/landing-pages/metamorfose/css/style.css") ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/css/style.css") ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -16,8 +17,7 @@
         <div class="logo-container">
             <div class="logo">
                 <a href="<?= base_url("metamorfose") ?>" style="color: inherit; text-decoration: none;">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Logo.png"); ?>" alt="Minha Logo"
-                        class="logo-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Logo.png"); ?>" alt="Minha Logo" class="logo-image">
                 </a>
             </div>
             <div class="light" id="light"></div>
@@ -40,26 +40,22 @@
         <div class="content-container">
             <div class="content-box">
                 <button onclick="window.location.href='#informacoesSAP';" class="custom-button">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnInformacoesSAP 2.png"); ?>"
-                        alt="Imagem 1" class="button-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnInformacoesSAP 2.png") ?>" alt="Imagem 1" class="button-image">
                 </button>
             </div>
             <div class="content-box">
                 <button onclick="window.location.href='#manualUso';" class="custom-button">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnManualUso 2.png"); ?>"
-                        alt="Imagem 2" class="button-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnManualUso 2.png") ?>" alt="Imagem 2" class="button-image">
                 </button>
             </div>
             <div class="content-box">
                 <button onclick="window.location.href='#comoAcessar';" class="custom-button">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnComoAcessar 1.png"); ?>"
-                        alt="Imagem 3" class="button-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnComoAcessar 1.png") ?>" alt="Imagem 3" class="button-image">
                 </button>
             </div>
             <div class="content-box">
                 <button onclick="window.location.href='#centralSuporte';" class="custom-button">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnCentralSuporte 1.png"); ?>"
-                        alt="Imagem 4" class="button-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/BtnCentralSuporte 1.png") ?>" alt="Imagem 4" class="button-image">
                 </button>
             </div>
         </div>
@@ -70,8 +66,7 @@
         <div id="contador"></div>
         <div id="video-container" style="display: none;">
             <video id="video" width="680" height="480" controls>
-                <source src="<?= base_url("_assets/landing-pages/metamorfose/video/VT-MissaoMeta_bx.mp4"); ?>"
-                    type="video/mp4">
+                <source src="<?= base_url("_assets/landing-pages/metamorfose/video/VT-MissaoMeta_bx.mp4"); ?>" type="video/mp4">
             </video>
             <button id="play-button" style="display: none;">
                 <i class="fas fa-play"></i>
@@ -91,36 +86,29 @@
         </p>
         <h3 class="Textoapresentacao_titulo">O futuro já chegou!</h3>
         <div id="informacoesSAP">
-            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png"); ?>" alt="Separacao01"
-                class="imagem-ajuste">
+            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png") ?>" alt="Separacao01" class="imagem-ajuste">
         </div>
     </div>
 
     <!-- Sobre o SAP -->
     <div class="SobreSap">
         <h1 class="SobreSap_Titulo">Sobre o SAP</h1>
-        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/TextoMaior01.png"); ?>" alt="textoMaior"
-            class="Tesxte_Maior">
-        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/TextoMaior.png"); ?>" alt="texto_Maior"
-            class="Tesxte_Maior1">
+        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/TextoMaior01.png") ?>" alt="textoMaior" class="Tesxte_Maior">
+        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/TextoMaior.png") ?>" alt="texto_Maior" class="Tesxte_Maior1">
     </div>
 
-    <!-- Cronograma -->
+    <!-- Cronograma 
     <div class="cronograma">
         <h1 class="cronograma">Cronograma</h1>
         <p class="texto_2">
-            Esse cronograma é nossa estratégia detalhada para garantir uma transição suave para o novo sistema SAP. Este
-            plano inclui todas as etapas necessárias para migrar dados e processos do sistema antigo para o novo,
-            garantindo que tudo funcione perfeitamente desde o primeiro dia.
+            Esse cronograma é nossa estratégia detalhada para garantir uma transição suave para o novo sistema SAP. Este plano inclui todas as etapas necessárias para migrar dados e processos do sistema antigo para o novo, garantindo que tudo funcione perfeitamente desde o primeiro dia.
         </p>
-        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Group 2.png"); ?>" alt="local_cronograma"
-            class="local_cronograma1">
-    </div>
+        <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Group 2.png") ?>" alt="local_cronograma" class="local_cronograma1">
+    </div> -->
     <!-- Manual SAP -->
     <div class="cotender_sap">
         <div class="imagem">
-            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png"); ?>" alt="separacao02"
-                class="local_manual_sap">
+            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png") ?>" alt="separacao02" class="local_manual_sap">
         </div>
         <div class="titulo">
             <div id="manualUso">
@@ -140,10 +128,8 @@
         </div>
         <div class="botoes">
             <div>
-                <a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Navegação Básica SAP GUI.pdf"); ?>"
-                    target="_blank">
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium.png"); ?>"
-                        alt="Visualizar Manual">
+                <a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Navegação Básica SAP GUI.pdf"); ?>" target="_blank">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium.png") ?>" alt="Visualizar Manual">
                 </a>
             </div>
             <div>
@@ -157,50 +143,37 @@
             </div>
             <!-- Botão que abrirá o pop-up -->
             <a id="open-popup" href="#">
-                <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium2.png"); ?>"
-                    alt="Download Glossário" class="Download1">
+                <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium2.png") ?>" alt="Download Glossário" class="Download1">
             </a>
             <div id="popup" class="popup">
                 <div class="popup-content">
                     <span id="close-popup" class="close-popup">&times;</span>
                     <h1 class="titulopopup">Glossário SAP</h1>
-                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png"); ?>"
-                        alt="Glossário SAP" class="popup-image">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png") ?>" alt="Glossário SAP" class="popup-image">
                     <ul class="download-menu">
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - BP.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - BP.pdf"); ?>" target="_blank">Glossário
                                 - BP</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - CO.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - CO.pdf"); ?>" target="_blank">Glossário
                                 - CO</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - CS.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - CS.pdf"); ?>" target="_blank">Glossário
                                 - CS</a></li>
                         <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - EWM e WM.pdf"); ?>"
                                 target="_blank">Glossário - EWM e WM</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - FI.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - FI.pdf"); ?>" target="_blank">Glossário
                                 - FI</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - MM.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - MM.pdf"); ?>" target="_blank">Glossário
                                 - MM</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PM.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PM.pdf"); ?>" target="_blank">Glossário
                                 - PM</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PP.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PP.pdf"); ?>" target="_blank">Glossário
                                 - PP</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PS.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - PS.pdf"); ?>" target="_blank">Glossário
                                 - PS</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - QM.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - QM.pdf"); ?>" target="_blank">Glossário
                                 - QM</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - SD.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - SD.pdf"); ?>" target="_blank">Glossário
                                 - SD</a></li>
-                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - TM.pdf"); ?>"
-                                target="_blank">Glossário
+                        <li><a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Glossário SAP - TM.pdf"); ?>" target="_blank">Glossário
                                 - TM</a></li>
                     </ul>
                 </div>
@@ -221,8 +194,7 @@
                 <div class="box_container">
                     <div class="box_acessar">
                         <div class="container">
-                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png"); ?>"
-                                alt="como_acessar" class="como_acessar1">
+                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png") ?>" alt="como_acessar" class="como_acessar1">
                             <div class="text-center">
                                 <h4>01</h4>
                                 <p>Ao acessar o sistema SAP, você será solicitado a inserir sua chave de acesso.</p>
@@ -231,8 +203,7 @@
                     </div>
                     <div class="box_acessar">
                         <div class="container">
-                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png"); ?>"
-                                alt="como_acessar" class="como_acessar1">
+                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png") ?>" alt="como_acessar" class="como_acessar1">
                             <div class="text-center">
                                 <h4>02</h4>
                                 <p>Digite o código que você recebeu.</p>
@@ -241,8 +212,7 @@
                     </div>
                     <div class="box_acessar">
                         <div class="container">
-                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png"); ?>"
-                                alt="como_acessar" class="como_acessar1">
+                            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Base3.png") ?>" alt="como_acessar" class="como_acessar1">
                             <div class="text-center">
                                 <h4>03</h4>
                                 <p>Após a validação, você terá acesso ao sistema SAP.</p>
@@ -257,25 +227,41 @@
     <!-- Central de Suporte -->
     <div id="centralSuporte">
         <div class="central_de_suporte">
-            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png"); ?>" alt="central"
-                class="local_central">
+            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/Separacao01.png") ?>" alt="central" class="local_central">
             <h1 class="central_titulo">Central de Suporte</h1>
             <p class="central_texto">
                 Durante a implementação do sistema SAP, nossa Central de Suporte será sua linha direta para resolver
                 qualquer dúvida ou problema que possa surgir. Nossa equipe de especialistas está pronta para fornecer
                 todo o suporte necessário para garantir o sucesso da missão.
             </p>
-            <h5 class="central_texto2">Em breve, você poderá conferir todos os contatos aqui.</h5>
+            <!--<h5 class="central_texto2">Em breve, você poderá conferir todos os contatos aqui.</h5> -->
+        </div>
+
+        <div id="Botão1">
+            <div class="guia_suporte">
+                <h1 class="central_titulo">Guia de Suporte</h1>
+                <a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Guia de Suporte para Usuários SAP.pptx"); ?>" target="_blank">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium.png") ?>" alt="Suporte" class="guia_suporte">
+                </a>
+
+            </div>
+        </div>
+        <div id="Botão2">
+            <div class="guia_suporte2">
+                <h1 class="central_titulo">Lista de Contatos</h1>
+                <a href="<?= base_url("_assets/landing-pages/metamorfose/manual/Projeto CoperSAP - Contatos Agentes e Local Operação Assistida.xlsx"); ?>"
+                    target="_blank">
+                    <img src="<?= base_url("_assets/landing-pages/metamorfose/images/ButtonsMedium2.png") ?>" alt="Suporte" class="guia_suporte">
+                </a>
+            </div>
         </div>
     </div>
 
     <!-- Footer -->
     <div class="footer-container">
         <div class="footer-images">
-            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/02.png"); ?>" alt="Imagem 02"
-                class="imagem02">
-            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/05.png"); ?>" alt="Imagem 03"
-                class="imagem03">
+            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/02.png") ?>" alt="Imagem 02" class="imagem02">
+            <img src="<?= base_url("_assets/landing-pages/metamorfose/images/05.png") ?>" alt="Imagem 03" class="imagem03">
         </div>
         <div class="footer-bottom">
             <p>&copy; ACME Copercana. Todos os direitos reservados</p>
@@ -284,9 +270,12 @@
 
 
         <!-- Scripts -->
-        <script src="<?= base_url("_assets/landing-pages/metamorfose/js/luz.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/luz.js"); ?>"></script>
-        <script src="<?= base_url("_assets/landing-pages/metamorfose/js/PopUp_Glossário.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/PopUp_Glossário.js"); ?>"></script>
-        <script src="<?= base_url("_assets/landing-pages/metamorfose/js/Regressiva.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/Regressiva.js"); ?>"></script>
+        <script
+            src="<?= base_url("_assets/landing-pages/metamorfose/js/luz.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/luz.js"); ?>"></script>
+        <script
+            src="<?= base_url("_assets/landing-pages/metamorfose/js/PopUp_Glossário.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/PopUp_Glossário.js"); ?>"></script>
+        <script
+            src="<?= base_url("_assets/landing-pages/metamorfose/js/Regressiva.js"); ?>?v=<?= filemtime("_assets/landing-pages/metamorfose/js/Regressiva.js"); ?>"></script>
 </body>
 
 </html>
